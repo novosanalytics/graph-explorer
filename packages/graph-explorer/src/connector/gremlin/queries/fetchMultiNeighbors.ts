c8db58e3b000a333eca29dec9d9d0f233cb09ce5
davinci07 <lysov.nikita@gmail.com>
2024-02-22 11:24:15 -0600

    rewriting how edges and vertx are collected

--- packages/graph-explorer/src/connector/gremlin/queries/fetchMultiNeighbors.ts
+++ packages/graph-explorer/src/connector/gremlin/queries/fetchMultiNeighbors.ts
@@ -36,17 +36,22 @@ const fetchMultiNeighbors = async (
   const gremlinTemplate = oneHopTemplate({ ...req, idType });
   const data = await gremlinFetch<RawOneHopRequest>(gremlinTemplate);
   console.log(`Node Query: ${gremlinTemplate}`)
-  const verticesResponse =
-    data.result.data["@value"]?.[0]?.["@value"][1]["@value"];
+  //const verticesResponse =
+  //  data.result.data["@value"]?.[0]?.["@value"][1]["@value"];
   //const verticesIds = verticesResponse?.map(v => toStringId(v["@value"].id));
   
   const rawVertices = data.result.data["@value"]
   let verticesIds: Array<any>[] = [];
   let edges: Edge[] = [];
+  const vertices: NeighborsResponse["vertices"] = [];
   rawVertices.forEach(vResult => {
-    let vDetails = vResult["@value"][1]["@value"].map(v => toStringId(v["@value"].id));
-    verticesIds.push(vDetails)
-    let eResult = vResult["@value"][3]["@value"]
+    const vItems = vResult["@value"][1]["@value"];
+    const vDetails = vItems.map(v => toStringId(v["@value"].id));
+    verticesIds.push(vDetails);
+    vertices.concat(vItems.map(
+      vertex => mapApiVertex(vertex)
+    ));
+    const eDetails = vResult["@value"][3]["@value"]
     .map(e=> {
       return mapApiEdge(e);
     }).filter(
@@ -54,31 +59,15 @@ const fetchMultiNeighbors = async (
         vDetails.includes(edge.data.source) ||
         vDetails.includes(edge.data.target)
     )
-    edges.concat(eResult)
+    console.log(`V Details: ${vDetails}`);
+    console.log(`E Details: ${eDetails}`);
+    edges.concat(eDetails)
   });
   //const rawVerticesIds = rawVertices?.map(v => toStringId(v["@value"].id));
-  console.log(`RAW DATA: ${verticesIds}`);
-  const vertices: NeighborsResponse["vertices"] = verticesResponse?.map(
-    vertex => mapApiVertex(vertex)
-  );
+  //const vertices: NeighborsResponse["vertices"] = verticesResponse?.map(
+  //  vertex => mapApiVertex(vertex)
+  //);
 
-
-  /*const edges = data.result.data["@value"]?.[0]?.["@value"][3]["@value"]
-    .map(value => {
-      return mapApiEdge(value);
-    })
-    .filter(
-      edge =>
-        verticesIds.includes(edge.data.source) ||
-        verticesIds.includes(edge.data.target)
-    );
-  */
-  let vString = '';
-  vertices.forEach(vertex => {
-    vString += `${vertex.data.id},`
-  })
-  console.log(`Vertices: ${vString}`);
-  console.log(`Edges: ${edges}`);
   return {
     vertices,
     edges,
