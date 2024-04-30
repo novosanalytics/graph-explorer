62822ec2b39ee0b9ecba09a5580ddc675aed89a5
davinci07 <lysov.nikita@gmail.com>
2024-04-30 15:30:57 -0500

    adding subgraph within new format

--- packages/graph-explorer/src/core/ConnectorProvider/types.ts
+++ packages/graph-explorer/src/core/ConnectorProvider/types.ts
@@ -1,5 +1,5 @@
 import LoggerConnector from "../../connector/LoggerConnector";
-import { CountsByTypeResponse, KeywordSearchResponse, NeighborsCountResponse, NeighborsResponse, SchemaResponse } from "../../connector/useGEFetchTypes";
+import { CountsByTypeResponse, KeywordSearchResponse, NeighborsCountResponse, NeighborsResponse, SchemaResponse, SubGraphResponse } from "../../connector/useGEFetchTypes";
 
 export type Explorer = {
   fetchSchema: (options?: any) => Promise<SchemaResponse>;
@@ -9,6 +9,7 @@ export type Explorer = {
   fetchEdgeNeighbors: (req: any, options?: any) => Promise<NeighborsResponse> 
   fetchMultiNeighbors: (req: any, options?: any) => Promise<NeighborsResponse>
   fetchNeighborsCount: (req: any, options?: any) => Promise<NeighborsCountResponse>
+  createSubgraph: (req: any, options?: any) => Promise<SubGraphResponse>
   keywordSearch: (req: any, options?: any) => Promise<KeywordSearchResponse>
 };
 
