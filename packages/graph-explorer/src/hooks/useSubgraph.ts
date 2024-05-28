0cee3f12314dbf06a7a28cf4d48f2245e10f3916
davinci07 <lysov.nikita@gmail.com>
2024-05-28 15:08:01 -0500

    setup with neighbor counts for subgraph

--- packages/graph-explorer/src/hooks/useSubgraph.ts
+++ packages/graph-explorer/src/hooks/useSubgraph.ts
@@ -1,10 +1,11 @@
 import { useCallback } from "react";
 import { useNotification } from "../components/NotificationProvider";
-import type { SubGraphRequest } from "../connector/useGEFetchTypes";
+import type { NeighborsRequest, SubGraphRequest } from "../connector/useGEFetchTypes";
 import { explorerSelector } from "../core/connector";
 import useEntities from "./useEntities";
 import { Vertex, Edge } from "../@types/entities";
 import { useRecoilValue } from "recoil";
+import { NeighborsCountRequest } from "../connector/useGEFetchTypes";
 
 
 const useSubGraph = () => {
@@ -12,7 +13,73 @@ const useSubGraph = () => {
   const explorer = useRecoilValue(explorerSelector);
   const { enqueueNotification, clearNotification } = useNotification();
 
-  return useCallback(
+
+  return useCallback( 
+    async (req: SubGraphRequest) => {
+        const subGraphResult = await explorer?.createSubgraph(req);
+
+        if (!subGraphResult || !subGraphResult.vertices.length) {
+            enqueueNotification({
+              title: "No Results",
+              message: "Your search has returned no results",
+            });
+            return;
+        }
+
+        setEntities({
+            nodes: subGraphResult.vertices,
+            edges: subGraphResult.edges,
+            selectNewEntities: "nodes",
+            forceSet:true
+        });
+
+        const notificationId = enqueueNotification({
+            title: "Filtering by Date",
+            message: `Looking for everything filtered on ${req.date}`,
+            autoHideDuration: null,
+          });
+          clearNotification(notificationId);
+
+        const verticesWithUpdatedCounts = await Promise.all<Vertex>(
+            subGraphResult.vertices.map(async vertex => {
+                const neighborsCount = await explorer?.fetchNeighborsCount({
+                    vertexId: vertex.data.id,
+                    idType: vertex.data.idType,
+                });
+
+                return {
+                    ...vertex,
+                    data: {
+                      ...vertex.data,
+                      neighborsCount:
+                        neighborsCount?.totalCount ?? vertex.data.neighborsCount,
+                      neighborsCountByType:
+                        neighborsCount?.counts ?? vertex.data.neighborsCountByType,
+                    },
+                  };
+            })
+        );
+
+        setEntities(prev => ({
+            nodes: prev.nodes.map(node => {
+              const nodeWithCounts = verticesWithUpdatedCounts.find(
+                v => v.data.id === node.data.id
+              );
+    
+              if (!nodeWithCounts) {
+                return node;
+              }
+    
+              return nodeWithCounts;
+            }),
+            edges: subGraphResult.edges,
+            forceSet:true
+          }));
+
+    }, [explorer, setEntities, enqueueNotification, clearNotification]
+  );
+
+  /*return useCallback(
     async (req: SubGraphRequest) => {
         const result = await explorer?.createSubgraph(req);
         
@@ -23,11 +90,6 @@ const useSubGraph = () => {
             });
             return;
         }
-        /*setEntities({
-            nodes: [],
-            edges: [],
-            forceSet: true,
-        });*/
 
         setEntities({
           nodes: result.vertices,
@@ -46,7 +108,9 @@ const useSubGraph = () => {
         
 
     },[explorer, setEntities, enqueueNotification, clearNotification]
-  );
-};
+    );*/
+
+}; 
+
 
 export default useSubGraph;
