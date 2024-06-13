9956736b53721f9f44272fdd3c1fde42cc7f2cba
davinci07 <lysov.nikita@gmail.com>
2024-06-13 11:30:13 -0500

    why does it take 6 files to do a simple search

--- packages/graph-explorer/src/hooks/useMultiQueryFetch.ts
+++ packages/graph-explorer/src/hooks/useMultiQueryFetch.ts
@@ -1,17 +1,83 @@
 import { useCallback } from "react";
 import { useNotification } from "../components/NotificationProvider";
-import type { NeighborsRequest } from "../connector/useGEFetchTypes";
+import type { MultiKeywordSearchRequest, NeighborsCountRequest } from "../connector/useGEFetchTypes";
 import { explorerSelector } from "../core/connector";
 import useEntities from "./useEntities";
 import { Vertex } from "../@types/entities";
 import { useRecoilValue } from "recoil";
+
 
 const useMultiQueryFetch = () => {
   const [, setEntities] = useEntities();
   const explorer = useRecoilValue(explorerSelector);
   const { enqueueNotification, clearNotification } = useNotification();
 
-  return useCallback(
+  const fetchNeighborsCount = useCallback(
+    (req: NeighborsCountRequest) => {
+      return explorer?.fetchNeighborsCount(req);
+    },
+    [explorer]
+  );
+
+  const fetchNodeResults = useCallback(
+
+  );
+
+    return useCallback(
+        async (req: MultiKeywordSearchRequest, limit: number, clusiver: string) => {
+
+
+            if (clusiver == "OR"){
+                // 
+            } else {
+                // Default: Add to keywordSearch, return the
+            }
+
+
+            const nodeOrNodes = null;
+            const nodes = Array.isArray(nodeOrNodes) ? nodeOrNodes : [nodeOrNodes];
+            const searchResults = await Promise.all(
+                nodes.map(async node => {
+                  const neighborsCount = await fetchNeighborsCount({
+                    vertexId: node.data.id,
+                    idType: node.data.idType,
+                    limit: neighbors_limit,
+                  });
+                  if (!neighborsCount) {
+                    return;
+                  }
+
+                  return {
+                    data: {
+                      ...node.data,
+                      neighborsCount:
+                        neighborsCount?.totalCount || node.data.neighborsCount,
+                      neighborsCountByType: {
+                        ...(node.data.neighborsCountByType || {}),
+                        ...(neighborsCount.counts || {}),
+                      },
+                    },
+                  };
+                })
+            );
+
+            const validResults = searchResults.filter(Boolean) as Vertex[];
+
+            if (!validResults.length) {
+              return;
+            }
+
+
+            setEntities({
+                nodes: validResults,
+                edges: [],
+                selectNewEntities: "nodes",
+              });
+        },
+        [explorer, setEntities, enqueueNotification, clearNotification]
+    );
+
+  /*return useCallback(
     async (req: NeighborsRequest) => {
       const result = await explorer?.fetchNeighbors(req);
 
@@ -73,6 +139,7 @@ const useMultiQueryFetch = () => {
     },
     [explorer, setEntities, enqueueNotification, clearNotification]
   );
+  */
 };
 
 export default useMultiQueryFetch;
